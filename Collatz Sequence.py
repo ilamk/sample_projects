e7c7f6fc14221b69e87e6d147905b3a489f9a6a5
ilamk <74969676+ilamk@users.noreply.github.com>
2021-12-22 12:29:06 +0800

    Update and rename Collatz Sequence.ipynb to Collatz Sequence.py

---
+++ Collatz Sequence.py
@@ -0,0 +1,21 @@
+def collatz(number):
+  global result
+  if number % 2 == 0:
+    print(number, ' // 2')
+    result = number // 2
+    return result
+  elif number % 2 == 1:
+    print('3 * ', number, ' + 1')
+    result = 3 * number + 1
+    return result
+
+while True:
+  try:
+    n = int(input('Enter a number: '))
+  except:
+    print("Try again")
+    continue
+  collatz(n)
+  if result == 1:
+    print('End')
+    break
